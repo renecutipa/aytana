8348f5372ecfdd294e60f6eef53da0fb863f4702
Rene Cutipa Chambi <renecutipa@gmail.com>
2017-05-25 10:43:01 -0500

    Servicio de clientes

--- application/views/sale/checkout.php
+++ application/views/sale/checkout.php
@@ -7,24 +7,45 @@ if (isset($user->username)) {
 <div class="main-content">
 <!-- ----------------   CONTENIDO ----------------------- -->
 <fieldset>
-	<div class="form-group col-xs-6 col-sm-6">
-		<label>Documento</label>
-		<select class="form-control" id="sale_type">
-			<option value="1">NOTA DE VENTA</option>
-			<option value="2">TICKET DE VENTA</option>
-		</select>
+	<div>
+		<div class="form-group col-xs-6 col-sm-6">
+			<label>Documento</label>
+			<select class="form-control" id="sale_type">
+				<option value="1">NOTA DE VENTA</option>
+				<option value="2">TICKET DE VENTA</option>
+			</select>
+		</div>
+		<div class="form-group col-xs-5 col-sm-5">
+			<label>DNI/RUC</label>
+			<input id="sale_doc" name="dni_ruc" class="form-control" type="text" />
+		</div>
+		<div class="form-group col-xs-1 col-sm-1">
+			<label></label>
+			<span id="isClient" class="entypo-check btn 	btn-success"></span>
+		</div>
+		<div class="form-group col-xs-12">
+			<label>Nombre o Razon Social</label>
+			<input id="sale_name" name="name" class="form-control" type="text" />
+		</div>
+		<div class="form-group col-xs-6 col-sm-6">
+			<label>Direccion</label>
+			<input id="sale_address" name="address" class="form-control" type="text" />
+		</div>
+		<div class="form-group col-xs-6 col-sm-6">
+			<label>PUNTOS</label>
+			<input id="points" name="puntos" class="form-control" type="text" />
+		</div>
 	</div>
-	<div class="form-group col-xs-6 col-sm-6">
-		<label>Nombre o Razon Social</label>
-		<input id="sale_name" name="name" class="form-control" type="text" />
-	</div>
-	<div class="form-group col-xs-6 col-sm-6">
-		<label>Direccion</label>
-		<input id="sale_address" name="address" class="form-control" type="text" />
-	</div>
-	<div class="form-group col-xs-6 col-sm-6">
-		<label>DNI/RUC</label>
-		<input id="sale_doc" name="dni_ruc" class="form-control" type="text" />
+	<div style="text-align: center">* * * * * * * * * *</div>
+	<div>
+		<div class="form-group col-xs-6 col-sm-6">
+			<label>MONTO ENTREGADO</label>
+			<input id="monto_entregado" class="form-control" type="text" />
+		</div>
+		<div class="form-group col-xs-6 col-sm-6">
+			<label>VUELTO</label>
+			<input id="monto_entregado" class="form-control" type="text" />
+		</div>
 	</div>
 
 </fieldset>
